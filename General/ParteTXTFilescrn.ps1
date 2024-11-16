4c5f83d5f9c3b69d5f4cc37787fe55b71a22ad7a
Maximiliano DUva <techmax@adinet.com.uy>
2024-11-16 17:25:52 -0300

    Archvios agregados a General

---
+++ General/ParteTXTFilescrn.ps1
@@ -0,0 +1,44 @@
+﻿Param([Parameter(Mandatory=$true)][string]$path,[string]$targetFile)
+Import-Module PersonalTools
+
+$Servidor=""
+
+Write-Host Chequeando existencia del archivo destino...... $targetFile -ForegroundColor Yellow
+if (Test-Path -Path $targetFile) {
+    Write-Host El archivo ya existe...... $targetFile -ForegroundColor Yellow
+} else {
+    Write-Host Creando archivo destino...... $targetFile -ForegroundColor Yellow
+    Set-Content -Path $targetFile -Value "Servidor,FolderPath,SharePath,FileGroups"
+}
+
+if (Test-Path -Path $path) {
+    Write-Host Analizando archivo origen...... $path -ForegroundColor Yellow
+    Get-Content -Path $path | ForEach-Object {
+        $i=$i+1
+        Write-Host "Leyendo linea:" $i -ForegroundColor Yellow
+
+        if ($_ -match "File screens on machine") {
+            $Servidor=Get-Palabra -frase $_.ToString() -ubicacion 5  
+        }
+
+        if ($_ -match "File Screen Path:") {
+            $folderPath=Get-FileScreenPath -frase $_.ToString()  
+        }
+
+        if ($_ -match "Share Path:") {
+            $sharePath=Get-SharePath -frase $_.ToString()  
+        }
+
+         if ($_ -match "File Groups:") {
+            $fileGroups=Get-FileGroups -frase $_.ToString()  
+
+            $auxString=$Servidor+","+$folderPath+","+$sharePath+","+$fileGroups
+            Add-Content -path $targetFile -Value $auxString
+        }
+
+    }
+    
+
+} else {
+        Write-Host "No se encuentra el archivo"
+}
---
+++ General/TSL_Check.ps1
@@ -0,0 +1,34 @@
+﻿
+#Get-ItemProperty -Path "Registry::HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols"
+
+
+#HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.2\Client\DisabledByDefault
+
+#Get-ChildItem -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols" -Recurse | Select PSPath, PSChildName
+
+
+#CD HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols
+ 
+#Get-ChildItem -Recurse -Path . | Where-Object -Property Name -Like '*Browser*' | Select-Object -Property PSPath
+
+
+$key = "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols"
+$subkeys = Get-ChildItem -Path $key
+foreach ($subkey in $subkeys) {
+    Write-Output "Subkey: $($subkey.Name)"
+}
+break
+
+Get-ItemPropertyValue -Path "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\MyApplication" -Name "Version"
+
+
+# Get registry value powershell
+$key = "HKCU:\Software\MyNewKey"
+$value = "MyValueName"
+$data = Get-ItemProperty -Path $key -Name $value
+Write-Output "The value of $value is: $($data.$value)"
+
+
+CD HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion
+ 
+Get-ItemProperty 
